8aea5aaeba20c94b95c7496781b7c9728356249c
Nul Pas <manu@lunaeme.com>
2017-10-09 13:51:47 +0200

    Nul Pàs / Angular Toolpas v.0.9.26  · Transformed literals module into static module.  · Updated version.

---
+++ source/static/source.static.module.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  angular
+    /**
+     * @namespace static
+     * @memberof source
+     *
+     * @description
+     * Module static definition for manage static data in application like literals or config variables.
+     */
+    .module('source.static', []);
+})();
